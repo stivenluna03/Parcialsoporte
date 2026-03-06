9fff5095e2062bfe6ca1ddab3f171285d45eabe1
stivenluna03 <163923788+stivenluna03@users.noreply.github.com>
2026-03-06 22:01:22 +0100

    Delete api/api/app-api directory

--- api/api/app-api/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Profile from './App'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Profile />
-  </StrictMode>
-)
